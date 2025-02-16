2bd2f1ad66bd01848c9d96734bb54027f6d96f26
Alyona <alyonabe@yandex.ru>
2025-02-16 19:35:50 +0300

    hw10_2

--- Homeworks/src/main/java/ru/otus/java/hw10/Main.java
+++ Homeworks/src/main/java/ru/otus/java/hw10/Main.java
@@ -1,14 +1,15 @@
 package ru.otus.java.hw10;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.addPerson("125-56-45", "Антон Павлович Чехов");
-        phoneBook.addPerson("454-11-05", "Салтыков-Щедрин Михаил Евграфович");
-        phoneBook.addPerson("454-88-05", "Гюстав Флобер");
-        phoneBook.addPerson("00-33-1-42-96-70-00", "Гюстав Флобер");
+        phoneBook.addPerson("Антон Павлович Чехов", List.of("125-56-45"));
+        phoneBook.addPerson("Салтыков-Щедрин Михаил Евграфович", List.of("454-11-05"));
+        phoneBook.addPerson("Гюстав Флобер", List.of("454-88-05","00-33-1-42-96-70-00"));
         phoneBook.find("Гюстав Флобер");
-        phoneBook.containsPhoneNumber("454-11-05");
+        phoneBook.containsPhoneNumber("125-56-45");
     }
 }
--- Homeworks/src/main/java/ru/otus/java/hw10/PhoneBook.java
+++ Homeworks/src/main/java/ru/otus/java/hw10/PhoneBook.java
@@ -1,29 +1,46 @@
 package ru.otus.java.hw10;
+
 import java.util.*;
 
 public class PhoneBook {
-    Map<String, String> phoneBook = new HashMap<>();
-    public void addPerson(String phoneNumber, String fio) {
-        phoneBook.put(phoneNumber, fio);
+    Map<String, List<String>> phoneBook = new HashMap<>();
+
+    public void addPerson(String fio, List<String> phoneNumber) {
+        phoneBook.put(fio, phoneNumber);
     }
 
     public void find(String fio) {
-        if (phoneBook.containsValue(fio)) {
-            System.out.println("The phone number for this person: ");
-            for (String key : phoneBook.keySet()) {
-                String value = phoneBook.get(key);
-                if (Objects.equals(value, fio)) {
-                    System.out.println(key);
-                }
+        if (phoneBook.containsKey(fio)) {
+            System.out.println("The phone numbers for this person: ");
+            for (String phoneNumber : phoneBook.get(fio)) {
+                System.out.println(phoneNumber);
             }
-        } else System.out.println("No such person in the phone book");
+        } else {
+            System.out.println("No such person in the phone book");
+        }
     }
 
     public void containsPhoneNumber(String phoneNumber) {
-        if (phoneBook.containsKey(phoneNumber)) {
+        boolean result = false;
+        for (List<String> valueList : phoneBook.values()) {
+            for (String value : valueList) {
+                if (value.equals(phoneNumber)) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        if (result) {
             System.out.println("Such phone in the phone book");
-        } else System.out.println("No such phone in the phone book");
+        } else {
+            System.out.println("No such phone in the phone book");
+        }
     }
 }
+
+
+
+
+
 
 
