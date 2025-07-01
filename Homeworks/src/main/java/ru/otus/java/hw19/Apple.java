502fb468ee39fa663d261a9e409ab35d929f1741
Alyona <alyonabe@yandex.ru>
2025-07-01 17:54:58 +0300

    hw19

---
+++ Homeworks/src/main/java/ru/otus/java/hw19/Apple.java
@@ -0,0 +1,7 @@
+package ru.otus.java.hw19;
+
+public class Apple extends Fruit {
+    public Apple(int weigth) {
+        super(weigth);
+    }
+}
---
+++ Homeworks/src/main/java/ru/otus/java/hw19/Orange.java
@@ -0,0 +1,7 @@
+package ru.otus.java.hw19;
+
+public class Orange extends Fruit {
+    public Orange(int weigth) {
+        super(weigth);
+    }
+}
